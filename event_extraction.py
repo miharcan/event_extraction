1fe4b6f571e67623c2cd6256bf0b594cf5137760
miharc <mihael.arcan@gmail.com>
2025-10-28 11:27:15 +0000

    adding spacy for ner

--- event_extraction.py
+++ event_extraction.py
@@ -1,6 +1,12 @@
+import spacy
+
+#python -m spacy download en_core_web_sm
+nlp = spacy.load("en_core_web_sm")
+
+
 headlines = [
     "Massive explosion rocks Beirut port area",
-    "Thousands protest government corruption in the capital",
+    "Thousands protest government corruption in the capital of United States",
     "Earthquake of magnitude 6.7 hits Japan coast with massive explosion"
 ]
 
@@ -17,9 +23,15 @@ def extract_events(texts, event_dict):
         events = []
         for word, label in event_dict.items():
             if word in text.lower():
-                events.append({"event":label})
-        if events != False:
-            results.append({"headline": text, "events":events})
+                events.append(label)
+        
+        doc = nlp(text)
+        propn = []
+        for ent in doc.ents:
+            if ent.label_ == "GPE":
+                propn.append(ent.text)
+
+        results.append({"headline": text, "events":events, "propns":propn})
     return results
 
 print(extract_events(headlines, event_keywords))
